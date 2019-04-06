063e24b658a78c27c3861e05fce3c76c14d29b7e
guyKremer <42372467+guyKremer@users.noreply.github.com>
2019-04-06 17:47:38 +0300

    html+css basic

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+createMainBoard(8);
+
+function createMainBoard(size) {
+    
+    var table = "";
+    for(var i=0; i<size; i++){
+        
+       table += "</tr\n>"; 
+
+        for (var j=0; j<size; j++){
+            
+            table += "<td id="+i+j +" class = gameSquare>" + i + "</td\n>";
+        }
+        table += "</tr\n>";
+    }
+
+    document.querySelector("#mainBoard").innerHTML = table;
+}
